43c7160db3d4722c3678259e334279052d25ced2
Richard Loera <richexp9@gmx.com>
2018-01-15 23:32:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,7 +12,7 @@ def shipping_manifest
   the_manifest["whale bone corsets"] = 5
   the_manifest["oil paintings"] = 3
   the_manifest["porcelain vases"] = 2
-
+  the_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 end
 puts shipping_manifest
